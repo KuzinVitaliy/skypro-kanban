9d01b714e878374de2e61c8bead4fa5d79806cca
KuzinVitaliy <vkuzin@vk-soft.ru>
2025-04-05 21:49:59 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,25 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { cards } from "./data.js";
 import { Card } from "./components/Card/card.jsx";
-import { header } from "./components/Header/header.jsx";
-import { main } from "./components/Main/main.jsx";
+import { Header } from "./components/Header/header.jsx";
+import { Main } from "./components/Main/main.jsx";
+import { PopNewCard } from "./components/Popups/PopNewCard/PopNewCard.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div classname="content">
+    <>
       <div>
         <Header />
       </div>
-      <div>
-        <Main />
+      <div classname="content">
+        <div>
+          <Main />
+        </div>
       </div>
-    </div>
+      <PopNewCard />
+    </>
   );
 }
 
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -3,11 +3,13 @@ import "./logo.svg";
 export function Header() {
   return (
     <div className="header">
-      <div className="logo"></div>
-      <div className="rightPanel">
-        <div className="createTask">Создать новую задачу</div>
-        <div className="signUp">Ivan Ivanov</div>
-        <div className="header_menu"></div>
+      <div className="header_block">
+        <div className="logo"></div>
+        <div className="rightPanel">
+          <div className="createTask">Создать новую задачу</div>
+          <div className="signUp">Ivan Ivanov</div>
+          <div className="header_menu"></div>
+        </div>
       </div>
     </div>
   );
